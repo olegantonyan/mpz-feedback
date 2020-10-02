2bf58431dc09e4765a6afb9637a494601f9aaf67
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2020-10-02 08:04:52 +0200

    fix validations

--- models/feedback.rb
+++ models/feedback.rb
@@ -1,8 +1,8 @@
 require 'active_record/base'
 
 class Feedback < ActiveRecord::Base
-  validates :text, presence: true, length: { maximum: 16384 }
-  validates :author, length: { maximum: 512 }
+  validates :text, presence: true, length: { maximum: 32768 }
+  validates :author, length: { maximum: 256 }
   validates :sysinfo, length: { maximum: 4096 }
 
   scope :ordered, -> { order(created_at: :desc) }
