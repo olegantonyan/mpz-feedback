fd7fe59d13dc0c6489f535d29a20ecea69885cc8
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2020-10-02 08:43:47 +0200

    config ru

--- app.rb
+++ app.rb
@@ -2,17 +2,15 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require_relative 'models/feedback'
 
-class App < Sinatra::Base
-  get '/' do
-    erb :index, locals: { items: Feedback.ordered }
-  end
+get '/' do
+  erb :index, locals: { items: Feedback.ordered }
+end
 
-  post '/feedback' do
-    content_type('application/json')
-    hash = JSON.parse(request.body.read)
-    Feedback.create!(author: hash.fetch('author', ''), text: hash.fetch('text'), sysinfo: hash.fetch('sysinfo', ''))
-    201
-  rescue StandardError => e
-    [422, JSON.generate(error: e.message)]
-  end
+post '/feedback' do
+  content_type('application/json')
+  hash = JSON.parse(request.body.read)
+  Feedback.create!(author: hash.fetch('author', ''), text: hash.fetch('text'), sysinfo: hash.fetch('sysinfo', ''))
+  201
+rescue StandardError => e
+  [422, JSON.generate(error: e.message)]
 end
