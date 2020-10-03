be4d7394dcbd0acb17fc14586a6760f3691329b8
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2020-10-03 18:13:30 +0200

    cosmetics

--- models/notification.rb
+++ models/notification.rb
@@ -2,18 +2,19 @@ require 'telegram/bot'
 
 class Notification
   def initialize(telegram_api_key:, telegram_chat_id:)
-    @telegram_api_key = telegram_api_key
-    @telegram_chat_id = telegram_chat_id
+    @telegram_api_key = telegram_api_key.to_s
+    @telegram_chat_id = telegram_chat_id.to_s
   end
 
   def send_message(text)
-    return if telegram_api_key.nil? || telegram_api_key.empty? || telegram_chat_id.nil? || telegram_chat_id.empty?
+    return if telegram_api_key.empty? || telegram_chat_id.empty?
     Telegram::Bot::Client.run(telegram_api_key) do |bot|
       bot.api.send_message(chat_id: telegram_chat_id, text: text)
     end
     puts 'telegram message sent'
   rescue StandardError => e
     puts "telegram bot error: #{e}"
+    puts e.backtrace
   end
 
   private
